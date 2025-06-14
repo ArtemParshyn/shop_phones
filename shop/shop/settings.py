c239c2417161c8ee618a5b5141eff01f0afabca8
Artem Parshyn <parshyn4@gmail.com>
2025-06-15 00:46:41 +0300

    User models.py

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'back',
 ]
+
+AUTH_USER_MODEL = 'back.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
